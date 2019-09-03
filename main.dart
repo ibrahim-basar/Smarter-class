afd6714cb4b9affe3b1b268cb670c94197001d9d
ibrahim-basar <52051845+ibrahim-basar@users.noreply.github.com>
2019-09-03 11:25:53 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,6 +1,120 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(title: "RaisedButton", home: MainActivity()));
+
+void main() => runApp(MaterialApp(title: "Maincode", home: MyApp()));
+
+
+
+
+
+//Login Page Starts Here
+class MyApp extends StatelessWidget {
+  @override
+  
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: LoginScreen(context),
+    );
+    
+    }
+  }
+
+
+
+  Widget LoginScreen(BuildContext context){
+    TextStyle style = TextStyle(fontSize: 20.0, color: Colors.white);
+    TextField emailField = TextField(
+          obscureText: false,
+          style: style,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              hintText: "Email",
+              hintStyle: style,
+              fillColor: Color.fromARGB(255, 50, 50, 125),
+              filled: true,
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+        );
+    TextField passField = TextField(
+          obscureText: true,
+          style: style,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              hintText: "Password",
+              hintStyle: style,
+              fillColor: Color.fromARGB(255, 50, 50, 125),
+              filled: true,
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+        );
+    Material loginButton = Material(
+          //elevation: 1000.0,
+          borderRadius: BorderRadius.circular(30.0),
+          child: FlatButton(
+            color: Color.fromARGB(100, 0, 0, 125),
+            padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            onPressed: () 
+            {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MainActivity()),
+                );
+            },
+            child: Text("Login",
+              textAlign: TextAlign.center,
+              style: style.copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          ),
+          );
+    return Scaffold(
+        backgroundColor: Color.fromARGB(255, 15, 15, 40),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 50, 50, 125),
+          title: Column(
+            children: <Widget>[
+              Text('SmarterClass', style: style.copyWith(fontSize: 25)),
+              Text('by IRIS Technologies', style: style.copyWith(fontSize: 12)),
+              ]
+              )
+        ),
+        body: Center(
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+                        "assets/logo.png",
+                        scale: 9,
+                      ),
+            SizedBox(height: 45.0),
+            Container(width: 330,
+            child: emailField
+        ),
+            SizedBox(height: 25.0),
+            Container(width: 330,
+            child: passField
+            ),
+            SizedBox(height: 35.0),
+            Container(width: 180,
+            child: loginButton
+            ),
+            SizedBox(height: 30.0)
+          ],
+        ),
+      )
+      );
+  } // getLoginPage end
+
+
+
+
+
+
+
+
+
+//Main page Starts Here
 
 class MainActivity extends StatefulWidget {
   @override
@@ -8,14 +122,14 @@ class MainActivity extends StatefulWidget {
 }
 
 class _MainActivityState extends State {
-  String msg = 'Flutter RaisedButton example';
+  String msg = 'Flutter Rai';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow,
       appBar: AppBar(
-        title: Text('Raised Button'),
+        title: Text('Raised '),
       ),
       body: Container(
         child: Center(
@@ -30,7 +144,7 @@ class _MainActivityState extends State {
               widgets(),
               Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,children: <Widget>[
               RaisedButton(
-                child: Text("Rock & Roll"),
+                child: Text("Rock "),
                 onPressed: _changeText,
                 color: Colors.red,
                 textColor: Colors.yellow,
